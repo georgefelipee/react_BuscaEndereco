fbb82b3be59fe10ff1015365d5f612de96213edc
unknown <georgefelipe938@gmail.com>
2023-03-17 08:41:58 -0300

    Adionando botao e finalizando

--- src/home/index.jsx
+++ src/home/index.jsx
@@ -3,10 +3,11 @@ import "./style.css"
 
 function App() {
 
-const[ cep, setCepNumber] = useState('') // estado para buscar cep do INPUT.
+const[ cep, setCepNumber] = useState('') // estado para buscar VALOR do  cep do INPUT.
+const [button,setButton] = useState() // estado para atualizar a pagina com UseEfect
 const [cepData, setCepData] = useState({rua:'', bairro:'', cidade:'', error:''}) // estado para pegar informações do cep e adiciona-las.
 
-useEffect(()=> {        // useEffect Usado para toma vez que o INPUT ser modificado, realizar uma requisição 
+useEffect(()=> {        // useEffect Usado para toda vez que o BUTTON for apertado, realizar uma requisição 
   console.log('chamado')
     fetch(`https://viacep.com.br/ws/${cep}/json/`)
       .then(response => response.json())
@@ -17,7 +18,7 @@ useEffect(()=> {        // useEffect Usado para toma vez que o INPUT ser modific
           cidade: data.localidade
         })
       })
-      .catch(error =>  { console.error('Erro! Digite Novamente')
+      .catch(error =>  { console.error('Erro! Digite Novamente') // Caso a requisiçao apresente um ERRO, os valores atribuidos sao zerados e sem seguida apresentando uma mensagem de ERRO para o usuário
         setCepData({
           rua:'',
           bairro: '',
@@ -26,19 +27,25 @@ useEffect(()=> {        // useEffect Usado para toma vez que o INPUT ser modific
         })
       })
   
-},[cep,setCepNumber])
+},[button])
 
   return (
     <div className="conteiner">
       <h1>Busca Endereço</h1>
 
       <header>
-        <input 
-         placeholder="Digite seu cep" 
-         type="text"
-         onChange={e => setCepNumber(e.target.value)}
-         />
-
+        <section>
+          <input 
+           placeholder="Digite seu cep" 
+            type="text"
+            onChange={e => setCepNumber(e.target.value)}
+          />
+          <button 
+            onClick={e=> setButton(e.clientX)}
+          placeholder="Buscar">
+            Buscar
+          </button>
+        </section>
         <p>Rua: {cepData.rua}</p>
         <p>Bairro: {cepData.bairro}</p>
         <p>Cidade: {cepData.cidade}</p>
